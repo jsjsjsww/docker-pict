2b4d8c71b58dac7b483697bd01f367eae832227a
jsjsjsww <1320795199@qq.com>
2019-02-02 16:07:14 +0800

    modify input to json

--- src/main/java/com/neo/controller/DockerController.java
+++ src/main/java/com/neo/controller/DockerController.java
@@ -3,11 +3,18 @@ package com.neo.controller;
 import com.neo.domain.CTModel;
 import com.neo.domain.TestSuite;
 import com.neo.service.PICTMethod;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.util.List;
 
 @RestController
 @RequestMapping("/generation")
@@ -15,16 +22,32 @@ public class DockerController {
 	
     @RequestMapping(value = "/PICT", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     // PICT 3.5 version
-    public TestSuite ACTSGeneration(HttpServletRequest request) {
-        String p = request.getParameter("parameters");
-        String stren = request.getParameter("strength");
-        String value = request.getParameter("values");
-        int parameters = Integer.parseInt(p);
-        int strength = Integer.parseInt(stren);
-        String[] val = value.split(" ");
-        int[] values = new int[val.length];
+    public TestSuite PICTGeneration(HttpServletRequest request) {
+
+        BufferedReader br;
+        StringBuilder sb = null;
+        String reqBody = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(
+                    request.getInputStream()));
+            String line = null;
+            sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            reqBody = URLDecoder.decode(sb.toString(), "UTF-8");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = new JSONObject(reqBody);
+        int parameters = (Integer)jsonObject.get("parameters");
+        int strength = (Integer)jsonObject.get("strength");
+        JSONArray jsonArray = (JSONArray)jsonObject.get("values");
+        List valueList = jsonArray.toList();
+        int[] values = new int[valueList.size()];
         for(int i = 0; i < values.length; i++)
-            values[i] = Integer.parseInt(val[i]);
+            values[i] = (Integer)valueList.get(i);
         CTModel model = new CTModel(parameters, strength, values);
         PICTMethod.generateModelFile(model);
         TestSuite ts = PICTMethod.runPICT("PICT/model.txt", strength);
